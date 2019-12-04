d765f86847dcdf7c7ff558f65da823d63a9a32dc
zwarag <harrys.kavan@protonmail.com>
2019-12-04 23:34:24 +0100

    Day4 2Star

--- Day4/main.go
+++ Day4/main.go
@@ -9,6 +9,7 @@ const input = "245318-765747"
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -52,4 +53,52 @@ func part1() {
 		num++
 	}
 	fmt.Println(solutions)
+}
+
+func part2() {
+	solutions := 0
+	min := input[:6]
+	max := input[7:]
+	fmt.Println(min, max)
+	num, _ := strconv.Atoi(min)
+	numMax, _ := strconv.Atoi(max)
+
+	for num <= numMax {
+		candidate := strconv.Itoa(num)
+		b := true
+		firstdouble := true
+		occ := 0
+		lastChar := rune(' ')
+
+		for pos, char := range candidate {
+			if pos == 0 {
+				occ++
+			} else {
+				if lastChar == char {
+					occ++
+				} else {
+					if firstdouble && occ == 2 {
+						firstdouble = false
+					}
+					occ = 1
+				}
+				if pos == 5 {
+					if firstdouble && occ == 2 {
+						firstdouble = false
+					}
+				}
+				if lastChar > char {
+					b = false
+					break
+				}
+			}
+			lastChar = char
+		}
+		if b && !firstdouble {
+			solutions++
+		}
+
+		num++
+	}
+	fmt.Println("solutions", solutions)
 }
